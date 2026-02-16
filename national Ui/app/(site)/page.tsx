96d7ce2b6844e9af1f2d80e722ca424adb839e83
BrainADZ <web@brainadz.marketing>
2026-02-16 17:51:29 +0530

    change

--- national Ui/app/(site)/page.tsx
+++ national Ui/app/(site)/page.tsx
@@ -9,7 +9,7 @@ import HeroVideoSection from "@/sections/heroVideoSlider";
 import HowWeWorkSection from "@/sections/HowWeWorkSection";
 import IndustriesSection from "@/sections/IndustriesSection";
 import MdDeskSection from "@/sections/MdDeskSection";
-import ProductsSection from "@/sections/productsSection";
+import ProductsSection from "@/sections/ProductsSection";
 import TestimonialsSection from "@/sections/Testimonial";
 
 export const metadata: Metadata = {
